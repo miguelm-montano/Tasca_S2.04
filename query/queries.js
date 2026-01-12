dfb563adcadd67a7363ac9b155945f7fdf9b6991
Miguel Montaño <miiguel2m@gmail.com>
2026-01-12 13:29:23 +0100

    Test 5 first queries

--- query/queries.js
+++ query/queries.js
@@ -2,16 +2,16 @@
 db.restaurants.find({}, { _id: 0 })
 
 // 2. Mostrar el restaurant_id i name per tots els documents en la col·lecció Restaurants.
-
+db.restaurants.find({}, {_id: 0, restaurant_id: 1, name: 1})
 
 // 3. Mostrar el restaurant_id, name, borough i cuisine.
-
+db.restaurants.find({}, {_id: 0, restaurant_id: 1, name: 1, borough: 1, cuisine: 1})
 
 // 4. Mostrar restaurant_id, name, borough i zip code.
-
+db.restaurants.find({}, {_id: 0, restaurant_id: 1, name: 1, borough: 1, zipcode: 1})
 
 // 5. Mostrar tots els restaurants que estan en el Bronx.
-
+db.restaurants.find({"borough": "Bronx"})
 
 // 6. Mostrar els primers 5 restaurants que estan en el Bronx.
 
